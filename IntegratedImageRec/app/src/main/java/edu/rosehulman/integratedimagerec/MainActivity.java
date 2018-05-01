ba31d3a8b5ca533e794a35e8d79afc048976a14b
yuchnw <yuchn.w@gmail.com>
2018-05-01 14:01:48 -0400

    Prepare 3 placeholder views

---
+++ IntegratedImageRec/app/src/main/java/edu/rosehulman/integratedimagerec/MainActivity.java
@@ -0,0 +1,40 @@
+package edu.rosehulman.integratedimagerec;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewFlipper mViewFlipper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mViewFlipper = findViewById(R.id.my_view_flipper);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_next) {
+            mViewFlipper.showNext();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
